869c251db9e5c7582c5461c3ce78c6e9965a5dec
TweedleDee <wendyjin0628@gmail.com>
2016-04-24 20:48:52 +0900

    have a try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,7 +30,7 @@ public class Main {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "index.ftl");
+            return new ModelAndView(attributes, "db.ftl");
         }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
