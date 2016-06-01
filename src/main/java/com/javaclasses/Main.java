ee8df4e8847a80a559532c5d61b5a0c85edac5be
TweedleDee <wendyjin0628@gmail.com>
2016-06-01 17:53:29 +0900

    modified pom infomation and main class

--- src/main/java/com/javaclasses/Main.java
+++ src/main/java/com/javaclasses/Main.java
@@ -46,7 +46,7 @@ public class Main {
   //           Map<String, Object> attributes = new HashMap<>();
   //           attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "hometest_w.html");
+            // return new ModelAndView(attributes, "hometest_w.html");
   //       }, new FreeMarkerEngine());
 
   //   get("/db", (req, res) -> {
@@ -75,6 +75,6 @@ public class Main {
   //     }
   //   }, new FreeMarkerEngine());
 
-  // }
+  }
 
 }
