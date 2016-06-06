9a592bd78ecead2c265af29513bdb5aec26ebb81
TweedleDee <wendyjin0628@gmail.com>
2016-06-06 18:05:25 +0900

    pull test

--- src/main/java/com/javaclasses/MainClass.java
+++ src/main/java/com/javaclasses/MainClass.java
@@ -31,7 +31,7 @@ public class MainClass {
   @RequestMapping("/")
   @ResponseBody
   public String home() {
-        return "HELLO , HEROKU!!!! with Springboot";
+        return "HELLO , HEROKU!!!! ";
   }
 
   public static void main(String[] args) {
